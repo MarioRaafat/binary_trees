5636730d8c6bebcc2f2d6f0e5f695aece8add42f
MarioRaafat <mariorafat10@gmail.com>
2024-04-01 20:46:43 -0700

    task3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * void binary_tree_delete - delete the entire tree
+ *
+ * @tree: A pointer to the root node
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
