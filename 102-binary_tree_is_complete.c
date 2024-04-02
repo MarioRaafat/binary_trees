452644052747e456b01e479939e84b4574e32925
MarioRaafat <mariorafat10@gmail.com>
2024-04-02 16:07:36 -0700

    hard-adv

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_complete - Checks if a binary tree is complete.
+ * @tree: A pointer to the root node of the tree to traverse.
+ *
+ * Return: If the tree is NULL or not complete, 0.
+ *         Otherwise, 1.
+ *
+ * Description: Upon malloc failure, exits with a status code of 1.
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	return 0;
+}
