74a85bba1c8d2f4e2398b2eaea67462b7e97a0ba
MarioRaafat <mariorafat10@gmail.com>
2024-04-01 23:43:34 -0700

    task15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,10 +13,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		leaves += (tree->left && !tree->right || !tree->left && tree->right) ? 1 : 0;
+		leaves += ((tree->left && !tree->right) || (!tree->left && tree->right)) ? 1 : 0;
 		leaves += binary_tree_leaves(tree->left);
 		leaves += binary_tree_leaves(tree->right);
 	}
 
-	return (leaves > 0 ? 0 : 1);
+	return ((leaves > 0) ? 0 : 1);
 }
