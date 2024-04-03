a18ef1adbba134720a1c5aea78f9d0e84e867f71
MarioRaafat <mariorafat10@gmail.com>
2024-04-02 19:05:46 -0700

    IDK

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -10,16 +10,37 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *ptr = *tree;
+	bst_t *current, *new;
 
-	if (tree == NULL)
+	if (tree != NULL)
 	{
-		bst_t *new = binary_tree_node(ptr, value);
-
-		return (*tree = new);
+		current = *tree;
+		if (current == NULL)
+		{
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (*tree = new);
+		}
+		if (value < current->n)
+		{
+			if (current->left != NULL)
+				return (bst_insert(&current->left, value));
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (current->left = new);
+		}
+		if (value > current->n)
+		{
+			if (current->right != NULL)
+				return (bst_insert(&current->right, value));
+			new = binary_tree_node(current, value);
+			if (new == NULL)
+				return (NULL);
+			return (current->right = new);
+		}
 	}
-	if (value > ptr->n)
-		return (bst_insert(&ptr->right, value));
-	else
-		return (bst_insert(&ptr->left, value));
+
+	return (NULL);
 }
